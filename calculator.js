1c55ce452e5a5bb5a7150628642f346d1045589c
nathan-cozens <nathan@numina.co.nz>
2019-03-12 11:42:08 +1300

    pseudocode is done ish, everything i can think of

---
+++ calculator.js
@@ -0,0 +1,9 @@
+//An array to store inputs for later
+//string the array to store current input and display it
+//Function to update the display by change the the array and make it into a string
+
+//Function to delete all it will just reset the array
+//Function to go back one, it will just pop the last item off the array
+//Function to eval the array and out put the total
+//going to need event listeners for when a button is clicked
+//
